f63dfdc38492a0509b3bd2d6fa1e313cc65224b5
KAVYA REDDY GUMPULA <kavya251001@gmail.com>
2025-08-10 18:05:48 +0530

    Time: 1 ms (85.79%), Space: 40.7 MB (77.89%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String res = sortHelper(n);
+        for (int i = 0; i < 31; i++) {
+            if (sortHelper(1 << i).equals(res)) return true;
+        }
+        return false;
+    }
+
+    private String sortHelper(int x) {
+        char[] arr = String.valueOf(x).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
